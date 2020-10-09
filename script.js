625e9bc9734faec86961d26db0c33db7715163ef
ar787 <53258333+ar787@users.noreply.github.com>
2020-10-09 10:48:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 class Gladiator {
+
     constructor(name, health, power, speed) {
         this.name = name
         this.health = health
         this.power = power
         this.speed = speed
+        // this.interval = null
     }
 
     attackEnemyAt(index) {
         const otherGladiator = gladiators[index]
-        let time = setTimeout( () => {
+        setTimeout( () => {
+            if (otherGladiator.health <= 0) { return }
+
             otherGladiator.health -= this.power
-            otherGladiator.speed = this.speed * (otherGladiator.health / this.health) 
+            otherGladiator.speed = this.speed * (otherGladiator.health / this.health)
+
             if (otherGladiator.health <= 0) {
-               requireCeasersChoiceForGladiator(otherGladiator, index, time)
-            }else if(otherGladiator.health <= 30) {
+               requireCeasersChoiceForGladiator(otherGladiator, index)
+            } else if (otherGladiator.health <= 30) {
                 otherGladiator.speed *= 3;
             }
+
+            console.log(`gladiator ${this.name}  is attacking enemy with power  ${this.power} to  ${otherGladiator.name}`)
         }, 5/this.speed)
     }
+
+    // free() {
+    //     clearInterval(this.interval)
+    // }
+
 }
 
 function genereteGladiators() {
     let arr = [];
-    let name = faker.name.findName()
-    let power =  getRandomInt(1,6)
-    let speed =  getRandomInt(1,6)
     for (let i = 0; i < 5; ++i) {
+        let name = faker.name.findName()
+        let power = getRandomInt(1,6)
+        let speed = getRandomInt(1,6)
         arr.push(new Gladiator(name, 100,  power , speed))
+        console.log(name)
     }
     return arr;
 }
@@ -43,34 +56,37 @@ function randomGladiator() {
 
 function requireCeasersChoiceForGladiator(gladiator, index) {
     let ceasersChoice = confirm("👎" + "👍")
+    
     if (ceasersChoice) {
         gladiator.health = 50
+        console.log(`Caesar showed 👍 ${gladiator.name}`)
     } else {
+        console.log(`Caesar showed 👎 ${gladiator.name}`)
+        // gladiator.free()
+        // gladiator = null
         gladiators.splice(index, 1)
     }
     return ceasersChoice;
 }
 
 function start() {
-    let time = setInterval(()=> {
+    let time = setInterval(() => {
         
-    if (gladiators.length < 2) {
-        
-        console.log(gladiators[0].name)
-        clearInterval(time)
-    }
-
     for (let idx = 0; idx < gladiators.length; ++idx) {
-        const gladiator = gladiators[idx]
+        
+        if (gladiators.length < 2) {
+            console.log(`${gladiators[0].name} won the battle with health x ${gladiators[0].health}`)
+            clearInterval(time)
+        }
 
+        const gladiator = gladiators[idx]
         let randomIdx = idx
+
         while (randomIdx === idx && gladiators.length > 1) {
             randomIdx = getRandomInt(0, gladiators.length)
         }
 
-        if (randomIdx === idx) {
-            return
-        }
+        if (randomIdx === idx) { return }
 
         gladiator.attackEnemyAt(randomIdx)
     }
